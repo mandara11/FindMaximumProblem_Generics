e7948d96267c60e69cdfcda79445fe8aa4019345
mandara11 <mandaram2345@gmail.com>
2022-08-09 20:33:18 +0530

    Added:RefactorUsingGeneric

--- Program.cs
+++ Program.cs
@@ -4,23 +4,11 @@
     {
         public static void Main(string[] args)
         {
-            //UC1
-            //MaxIntNumber maximum1 = new MaxIntNumber();
-            //int value = maximum1.MaximumInteger(24, 44, 51);
+            GenericMaximum<int> maximum = new GenericMaximum<int>(24,44,99);
+            int value = maximum.MaximumInteger(24, 44, 99);
 
-            //Console.WriteLine("The Maximum Integer is: " + value);
+            Console.WriteLine("The Maximum Integer is : " +value);
 
-            //UC2
-            //MaxFloatNumber maximum2 = new MaxFloatNumber();
-            //double value = maximum2.MaximumFloatNumber(2.4, 4.4, 5.1);
-
-            //Console.WriteLine("The Maximum Integer is: " + value);
-
-            //UC3
-            MaxStringNumber maximum3 = new MaxStringNumber();
-            string str = maximum3.MaximumStringNumber("Apple","Peach","Banana");
-
-            Console.WriteLine("The MaximumString is: " + str);
         }
     }
 }
