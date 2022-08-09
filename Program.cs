6df81ece09220c5c396785717f9025e60bc0618c
mandara11 <mandaram2345@gmail.com>
2022-08-09 19:18:33 +0530

    Added:UC1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace FindMaximumProblemUsingGenerics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            MaxIntNumber maximum = new MaxIntNumber();
+            int value = maximum.MaximumInteger(24, 44, 51);
+
+            Console.WriteLine("The Maximum Integer is: " + value);
+        }
+    }
+}
