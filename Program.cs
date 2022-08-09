9c7165d77f55743042b8699899bd9d6e02ed1069
mandara11 <mandaram2345@gmail.com>
2022-08-09 19:29:17 +0530

    Added:UC2

--- Program.cs
+++ Program.cs
@@ -4,8 +4,15 @@
     {
         public static void Main(string[] args)
         {
-            MaxIntNumber maximum = new MaxIntNumber();
-            int value = maximum.MaximumInteger(24, 44, 51);
+            //UC1
+            //MaxIntNumber maximum = new MaxIntNumber();
+            //int value = maximum.MaximumInteger(24, 44, 51);
+
+            //Console.WriteLine("The Maximum Integer is: " + value);
+
+            //UC2
+            MaxFloatNumber maximum = new MaxFloatNumber();
+            double value = maximum.MaximumFloatNumber(2.4, 4.4, 5.1);
 
             Console.WriteLine("The Maximum Integer is: " + value);
         }
